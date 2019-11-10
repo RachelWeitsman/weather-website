be55e0a0dcd19577d67f9c967e4212af6dfb2ab5
Rachel Fisher <rachelf@sweetin.com>
2019-11-10 15:18:03 +0200

    add highest temperature details

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + 
+            body.currently.temperature + ' degrees out. There is a ' + 
+            body.currently.precipProbability + '% chance of rain. \nThe highest temperature today is ' + 
+            body.daily.data[0].temperatureHigh)
         }
     })    
 }
